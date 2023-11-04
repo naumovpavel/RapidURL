b7172783e779b662542245ab593f0a7f32bc3a19
naumovpavel <naumovpasha193@gmail.com>
2023-11-05 00:30:04 +0300

    update response logic

--- internal/api/http/response/response.go
+++ internal/api/http/response/response.go
@@ -1,24 +1,15 @@
 package response
 
 type Response struct {
-	Status string `json:"status"`
-	Error  string `json:"error,omitempty"`
+	Error string `json:"error,omitempty"`
 }
 
-const (
-	StatusOk    = "Ok"
-	StatusError = "Error"
-)
-
 func Ok() Response {
-	return Response{
-		Status: StatusOk,
-	}
+	return Response{}
 }
 
 func Error(err error) Response {
 	return Response{
-		Status: StatusError,
-		Error:  err.Error(),
+		Error: err.Error(),
 	}
 }
