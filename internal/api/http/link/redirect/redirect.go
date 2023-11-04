9cc65e583b6458274a83bbd01c44fbb05099b97c
naumovpavel <naumovpasha193@gmail.com>
2023-11-05 00:34:36 +0300

    update redirect handler

--- internal/api/http/link/redirect/redirect.go
+++ internal/api/http/link/redirect/redirect.go
@@ -15,13 +15,13 @@ import (
 	"github.com/go-chi/render"
 )
 
-type Geter interface {
+type Getter interface {
 	GetLink(dto link.GetLinkDTO) (url.URL, error)
 }
 
-func New(log *slog.Logger, gt Geter) http.HandlerFunc {
+func New(log *slog.Logger, gt Getter) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		const op = "api.http.link.redirect.New"
+		const op = "api.http.link.redirect"
 		log = log.With(
 			slog.String("op", op),
 			slog.String("request_id", middleware.GetReqID(r.Context())),
@@ -31,15 +31,19 @@ func New(log *slog.Logger, gt Geter) http.HandlerFunc {
 		redirectUrl, err := gt.GetLink(link.GetLinkDTO{Alias: alias})
 
 		if err != nil {
-			log.Error("failed to find link", sl.Err(err))
-			if errors.Is(err, storage.ErrLinkNotFound) {
-				render.JSON(w, r, response.Error(err))
-			} else {
-				render.JSON(w, r, response.Error(errors.New("internal error")))
-			}
+			handleLinkFindError(w, r, log, err)
 			return
 		}
 
 		http.Redirect(w, r, redirectUrl.String(), http.StatusFound)
+	}
+}
+
+func handleLinkFindError(w http.ResponseWriter, r *http.Request, log *slog.Logger, err error) {
+	log.Error("failed to find link", sl.Err(err))
+	if errors.Is(err, storage.ErrLinkNotFound) {
+		render.JSON(w, r, response.Error(err))
+	} else {
+		render.JSON(w, r, response.Error(errors.New("internal error")))
 	}
 }
