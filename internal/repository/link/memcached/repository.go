f88b5247ba235d6911d6d9d1715e13d04b572387
naumovpavel <naumovpasha193@gmail.com>
2023-11-06 04:50:13 +0300

    add context support to memcached queries

--- internal/repository/link/memcached/repository.go
+++ internal/repository/link/memcached/repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"RapidURL/internal/entity"
 	"RapidURL/internal/repository/link"
@@ -11,6 +12,7 @@ import (
 )
 
 const expiration = 60 * 60 * 24
+const timeout = 500 * time.Millisecond
 
 type Repository struct {
 	client *memcache.Client
@@ -22,18 +24,24 @@ func New(client *memcache.Client) *Repository {
 
 func (r *Repository) SaveLink(ctx context.Context, dto link.DTO) error {
 	const op = "repository.link.memcached.SaveLink"
+
+	r.setTimeOut(ctx)
 	json, err := json.Marshal(dto)
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
+
 	return r.client.Add(&memcache.Item{
 		Key:        dto.Alias,
 		Value:      json,
 		Expiration: expiration,
 	})
 }
+
 func (r *Repository) FindLinkByAlias(ctx context.Context, alias string) (entity.Link, error) {
 	const op = "repository.link.memcached.FindLinkByAlias"
+
+	r.setTimeOut(ctx)
 	item, err := r.client.Get(alias)
 	if err != nil {
 		return entity.Link{}, fmt.Errorf("%s: %w", op, err)
@@ -46,4 +54,13 @@ func (r *Repository) FindLinkByAlias(ctx context.Context, alias string) (entity.
 	}
 
 	return link.ToEntity(dto)
+}
+
+func (r *Repository) setTimeOut(ctx context.Context) {
+	deadline, _ := ctx.Deadline()
+	if time.Until(deadline) > 0 {
+		r.client.Timeout = time.Until(deadline)
+	} else {
+		r.client.Timeout = 0
+	}
 }
